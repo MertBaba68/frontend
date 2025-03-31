06b7db3fcf945cac59e6fe03562d55f4e7ada078
MertBaba68 <mert.unlu@student.hu.nl>
2025-03-31 13:54:10 +0200

    conifg update

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "vodafonebackend.azurewebsites.net"
+const API_BASE_URL = "vodafonebackend-fkdmcedchaa7e3a6.northeurope-01.azurewebsites.net"
 
 export default {
     API_BASE_URL,
